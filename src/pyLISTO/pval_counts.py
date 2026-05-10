4a3a1fcc354b4c70f375e85ac8192456e55fceea
Andrei Stoica <andreistoica@foxmail.com>
2026-05-10 16:41:26 +0800

    Added pval_counts_3n

--- src/pyLISTO/pval_counts.py
+++ src/pyLISTO/pval_counts.py
@@ -41,3 +41,66 @@ def pval_counts_2mn(
         pval += power_product(primes, exponents)
 
     return pval
+
+
+def pval_counts_3n(
+    len_a: int,
+    len_b: int,
+    len_c: int,
+    n: int,
+    k: int
+) -> float:
+    """
+    Compute the probability that three subsets of a set intersect
+    in at least k points.
+
+    Parameters
+    ----------
+    len_a : int
+        Size of the first subset.
+    len_b : int
+        Size of the second subset.
+    len_c : int
+        Size of the third subset.
+    n : int
+        Size of the set comprising the subsets.
+    k : int
+        Minimum size of the intersection.
+        
+    Returns
+    -------
+    float
+        A numeric value in [0, 1] representing the probability that
+        three subsets of a set intersect in at least k points.
+
+    Raises
+    ------
+    ValueError
+        If `k` exceeds the minimum length of the three subsets.
+
+    Examples
+    --------
+    >>> pval_counts_3n(300, 200, 250, 400, 180)
+    """
+    if k == 0:
+        return 1.0
+
+    lengths = sorted([len_a, len_b, len_c])
+
+    if k > lengths[0]:
+        raise ValueError(
+            "`k` must not exceed the minimum length of the three sets."
+        )
+
+    pval: float = 0.0
+
+    for x in range(k, lengths[0] + 1):
+        pval += prob_counts_3n(
+            lengths[0],
+            lengths[1],
+            lengths[2],
+            n,
+            x
+        )
+
+    return pval
